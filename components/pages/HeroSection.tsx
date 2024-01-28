f3f74cd3cec2007975aada2032cec8beb26f7744
Kristijan Federer <federer.kristijan@gmail.com>
2024-01-28 22:03:43 +0100

    animation wokring. need short interval and long pause in between intervals.

--- components/pages/HeroSection.tsx
+++ components/pages/HeroSection.tsx
@@ -4,40 +4,63 @@ import Foto from '@/assets/CV Foto Glasses 1 - Edited 1.png'
 import { Button } from '../ui/Button';
 import Moustache from '@/assets/moustache.png'
 import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
 const HeroSection = ({
   title,
   subtitle,
   image,
   }: HeroSectionPayload) => {
-    const [isRattling, setIsRattling] = useState(false);
+    const [rotation, setRotation] = useState(0);
 
     useEffect(() => {
-      const rattlingInterval = setInterval(() => {
-        setIsRattling(true);
-        setTimeout(() => setIsRattling(false), 500); // Adjust the duration of the rattle effect
-      }, 15000); // Set the interval for every 15 seconds
+      const intervalId = setInterval(() => {
+        // Rotate left and right slowly
+        setRotation((prevRotation) => (prevRotation >= 0 ? -5 : 5));
+      }, 500); // Set the duration for each half rotation to 0.5 seconds
 
-      return () => clearInterval(rattlingInterval);
+      // After 45 seconds, clear the interval to pause the animation
+      const pauseIntervalId = setInterval(() => {
+        clearInterval(intervalId);
+      }, 45000); // Set the pause duration to 45 seconds
+
+      // Clear the pause interval when the component unmounts
+      return () => {
+        clearInterval(intervalId);
+        clearInterval(pauseIntervalId);
+      };
     }, []);
 
   return (
     <div className='flex justify-between px-6'>
       <div className='p-20 relative'>
-        {/* {isRattling && (<div
-          className='moustache-container absolute top-[12%] left-[44%]'
-        >
-          <Image
-            src={Moustache}
-            alt="animation"
-          />
-        </div>)} */}
-        <Image
-            src={Moustache}
-            alt="animation"
-            className='absolute top-[12%] left-[44%]'
-          />
-        <h1 className='text-7xl py-4'>{title}</h1>
+        <div className=''>
+          <motion.div
+              // animate={{
+              //   rotate: rotation,
+              // }}
+              // transition={{
+              //   duration: 0.3, // One complete rotation in 45 seconds
+
+              //   ease: 'linear',
+              //   times: [0, 0.5, 1], // Keyframes for left rotation, default, and right rotation
+              // }}
+
+              animate={{ rotate: [rotation, 0, rotation], }}
+              transition={{
+                from: 60,
+                duration: 0.5,
+                ease: 'linear',
+              }}
+          >
+            <Image
+                src={Moustache}
+                alt="animation"
+                className='absolute top-[10%] left-[32%]'
+              />
+          </motion.div>
+          <h1 className='text-7xl py-4'>{title}</h1>
+        </div>
         <h2 className='text-5xl py-4'>{subtitle}</h2>
         <span className='text-3xl py-4'>I write clean code to create quality applications with intuitive user experience.</span>
         <div className='flex gap-8 py-12'>
